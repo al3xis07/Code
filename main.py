a544a2b4ad40598ac81c17068111ef9cbe71f85d
al3xis07 <alexisjanelle94@gmail.com>
2024-10-10 17:40:49 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+workspace git:(master) ls
+main.py  README.md  requirements.txt
+workspace git:(master) which
+workspace git:(master) which ls
+ls: aliased to ls --color=tty
+workspace git:(master) which pwd
+pwd: shell built-in command
+workspace git:(master) pwd
+/project/workspace
+  workspace git:(master) python
+Python 3.8.20 (default, Sep 12 2024, 21:11:49)
+[GCC 12.2.0] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>>quit()
+  workspace git:(master) which python
+/usr/local/bin/python
+  workspace git:(master) python --version
+  Python 3.8.20
+  workspace git:(master) pwd
+  /project/workspace
+  workspace git(master) cp main.py function.py
+  workspace git(master) ls
+  functions.py  main.py  README.md  requirments.txt
+  workspace git(master) 
